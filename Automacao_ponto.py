c97049d5e32678b7fc3c1030f82e2afb30945f9f
Penna <minuscloth96@gmail.com>
2025-08-31 19:53:50 -0300

    Adicionando envio de email

--- Automacao_ponto.py
+++ Automacao_ponto.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
 
 import smtplib
 from email.mime.text import MIMEText
@@ -35,24 +36,54 @@ driver.get(url)
 
 wait = WebDriverWait(driver,10)
 
-email_field = wait.until(EC.visibility_of_element_located((By.XPATH,'//*[@id="login_email"]')))
-password_field = wait.until(EC.visibility_of_element_located((By.XPATH,'//*[@id="login_password"]')))
-login_button = driver.find_element(By.XPATH,'//*[@id="root"]/div[2]/div/div/div/div/div/form/div[3]/div/div/span/button/span[2]')
+try:
+
+    email_field = wait.until(EC.visibility_of_element_located((By.XPATH,'//*[@id="login_email"]')))
+    password_field = wait.until(EC.visibility_of_element_located((By.XPATH,'//*[@id="login_password"]')))
+    login_button = driver.find_element(By.XPATH,'//*[@id="root"]/div[2]/div/div/div/div/div/form/div[3]/div/div/span/button/span[2]')
 
 
 
-email_field.send_keys(email)
-password_field.send_keys(password)
-login_button.click()
-close_popup = wait.until(EC.visibility_of_element_located((By.XPATH,'/html/body/div[2]/div/div[2]/div/div[2]/div/div[1]/div/div/span')))
-#/html/body/div[2]/div/div[2]/div/div[2]/div/div[1]/div/div/span
-#/html/body/div[2]/div/div[2]/div/div[2]/div/div[1]/div/div/span/span/svg
-close_popup.click()
+    email_field.send_keys(email)
+    password_field.send_keys(password)
+    login_button.click()
+    close_popup = wait.until(EC.visibility_of_element_located((By.XPATH,'/html/body/div[2]/div/div[2]/div/div[2]/div/div[1]/div/div/span')))
+    #/html/body/div[2]/div/div[2]/div/div[2]/div/div[1]/div/div/span
+    #/html/body/div[2]/div/div[2]/div/div[2]/div/div[1]/div/div/span/span/svg
+    close_popup.click()
 
-cpf_field = wait.until(EC.visibility_of_element_located((By.XPATH,'//*[@id="root"]/div[2]/div[2]/main/div/div/div/div/div/div[2]/div/div/div[1]/button')))
-cpf_field.send_keys(cpf)
+    cpf_field = wait.until(EC.visibility_of_element_located((By.XPATH,'//*[@id="root"]/div[2]/div[2]/main/div/div/div/div/div/div[2]/div/div/div[1]/button')))
+    cpf_field.click()
+    cpf_field_input= wait.until(EC.visibility_of_element_located((By.XPATH,'//*[@id="root"]/div[2]/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/input')))
+    cpf_field_input.send_keys(cpf)
 
+    #Falta apertar tecla enter ou clicar em bater
+    
 
+except :
 
+    # Configuração do e-mail
+    email_send = os.getenv('EMAIL_SEND')
+    email_user = os.getenv('EMAIL_USER')
+    email_password = os.getenv('EMAIL_PASSWORD')
+    subject = 'Falha ao bater o ponto'
+
+    data = datetime.now().strftime("%d-%m-%Y %H:%M:%S") 
+    body = f'Falha ao bater o ponto: {data}'
+
+    #COnfig servidor
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(email_user, email_password)
+
+    #Montar email
+    msg = MIMEMultipart()
+    msg['From'] = email_user
+    msg['To'] = email_send
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body, 'plain'))
+    server.sendmail(email_user, email_send, msg.as_string())
+
+    server.quit()
 
 
